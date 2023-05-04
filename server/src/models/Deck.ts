f11df935d59688dbfb791cb87c665350fcdf6bc8
Jorge Castro <david92_jc@yahoo.es>
2023-05-04 00:15:05 -0500

    correcciones menores

--- server/src/models/Deck.ts
+++ server/src/models/Deck.ts
@@ -5,14 +5,10 @@ export type Card = { text: string }
 
 export const createDeckTable = async () => {
     const client = await pool.connect();
-    
+
     try {
         await client.query(`
-      CREATE TABLE IF NOT EXISTS Deck (
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(255) NOT NULL,
-        cards TEXT[]
-      );
+      CREATE TABLE IF NOT EXISTS Deck (id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL, cards TEXT[]);
     `);
     } finally {
         client.release();
